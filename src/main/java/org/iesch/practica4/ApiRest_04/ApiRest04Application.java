ece2628883ddcbcdbcf471efa4a975662b907ab0
michel <mrubios@iesch.org>
2023-02-25 11:34:52 +0100

    Initial commit

---
+++ src/main/java/org/iesch/practica4/ApiRest_04/ApiRest04Application.java
@@ -0,0 +1,13 @@
+package org.iesch.practica4.ApiRest_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRest04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRest04Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesch/practica4/ApiRest_04/ApiRest04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesch.practica4.ApiRest_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRest04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
